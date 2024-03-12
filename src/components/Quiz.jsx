f8186b792f6c8e2f413e62428fee871d931ac0a8
Assaf <1214assaf@gmail.com>
2024-03-12 11:44:24 +0200

    shuffeling answers and add quiz complete logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizComplete from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-
   const activeQuestionIndex = userAnswers.length;
+  const isQuizOver = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -11,12 +12,24 @@ export default function Quiz() {
     });
   }
 
+  if (isQuizOver) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="completed" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const suffeledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffeledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffeledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
