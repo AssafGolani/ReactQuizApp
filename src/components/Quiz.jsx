b40ed8ffb1a3610b9ae4404faa21adfd6d54826a
Assaf <1214assaf@gmail.com>
2024-03-12 12:29:35 +0200

    Add QuestionTimer component and integrate it into Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
 import quizComplete from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
@@ -27,6 +28,10 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          timeout={10000}
+          onTimeout={() => handleSelectAnswer(null)}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {suffeledAnswers.map((answer) => (
