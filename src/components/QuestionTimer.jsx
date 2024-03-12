4f85fb1466c93f8191d2e5437240ddc2fbdada28
Assaf <1214assaf@gmail.com>
2024-03-12 12:45:11 +0200

    adding cleanup function to effects and resetting component with key

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -5,14 +5,21 @@ export default function QuestionTimer({ timeout, onTimeout }) {
 
   useEffect(() => {
     console.log("setTimeout");
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
     console.log("setInterval");
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
